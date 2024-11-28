203adaff0927624f8f4b24989e19efc97015cc92
Stream <1542763342@qq.com>
2024-11-29 06:32:14 +0800

    feat(StreamChatPage): add top bar

--- composeApp/src/commonMain/kotlin/io/github/stream29/remenberingassistant/view/page/StreamChat.kt
+++ composeApp/src/commonMain/kotlin/io/github/stream29/remenberingassistant/view/page/StreamChat.kt
@@ -9,7 +9,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import io.github.stream29.remenberingassistant.model.ApplicationContext
@@ -34,8 +35,13 @@ fun ApplicationContext.StreamChatPage(streamChatViewModel: StreamChatViewModel) 
             modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            if(onError)
+            if (onError)
                 FailDialog(errorMessage) { onError = false }
+            TopAppBar(
+                modifier = Modifier.fillMaxWidth().weight(0.1f),
+            ) {
+                Text("Stream Chat")
+            }
             LazyColumn(
                 reverseLayout = true,
                 modifier = Modifier.fillMaxWidth().weight(1f)
@@ -54,7 +60,7 @@ fun ApplicationContext.StreamChatPage(streamChatViewModel: StreamChatViewModel) 
                 }
             }
             Row(
-                modifier = Modifier.fillMaxWidth().weight(0.1f),
+                modifier = Modifier.fillMaxWidth().weight(0.2f),
                 verticalAlignment = Alignment.Bottom
             ) {
                 TextField(
